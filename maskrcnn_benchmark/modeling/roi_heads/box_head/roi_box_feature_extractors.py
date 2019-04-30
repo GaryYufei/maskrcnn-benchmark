b8a4ed5bf9b80b20a622e622ed37ab4789fde15a
garyyufei <garyyufei@gmail.com>
2019-04-30 23:40:04 +1000

    add another model design

--- maskrcnn_benchmark/modeling/roi_heads/box_head/roi_box_feature_extractors.py
+++ maskrcnn_benchmark/modeling/roi_heads/box_head/roi_box_feature_extractors.py
@@ -40,8 +40,8 @@ class ResNet50Conv5ROIFeatureExtractor(nn.Module):
         self.pooler = pooler
         self.head = head
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.out_channels = config.MODEL.ROI_BOX_HEAD.EMBEDDING_DIM if config.MODEL.ROI_BOX_HEAD.EMBEDDING_INIT else head.out_channels
-
+        # self.out_channels = config.MODEL.ROI_BOX_HEAD.EMBEDDING_DIM if config.MODEL.ROI_BOX_HEAD.EMBEDDING_INIT else head.out_channels
+        self.out_channels = head.out_channels
         # if config.MODEL.ROI_BOX_HEAD.EMBEDDING_INIT:
         #     self.tanh = nn.Tanh()
         #     self.embedding_fc = nn.Linear(head.out_channels, config.MODEL.ROI_BOX_HEAD.EMBEDDING_DIM)
